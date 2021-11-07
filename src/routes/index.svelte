4faebdd48468967f254b3b780002ef1710f6344b
academic-telebort <academic.telebort@gmail.com>
2021-11-07 08:28:04 +0000

    commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from '$lib/Header.svelte';
+    import Section from '$lib/Section.svelte';
+
+    let myInfo = {
+        title:"Info",
+        item1:"Naz",
+        item2:15,
+        item3:"MBS"
+    }
+
+    let myHobby = {
+        title:"Hobby",
+        item1:"Playing games",
+        item2:"Reading books",
+        item3:"Play badminton"
+    }
+</script>
+
+<Header title={"My Biography"} />
+<Section {...myInfo} />
+<Section {...myHobby} />
